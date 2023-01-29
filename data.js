92dba79822cde3868725f6c2f939c26260c82a53
roman <roman3668@gmail.com>
2023-01-29 16:57:06 +0300

    HW Lesson 10

---
+++ data.js
@@ -0,0 +1,19 @@
+const dataInfo = `[{
+        "id": "octavia",
+        "url": "https://skodasite.ru/pictures/internal/octaviaa5g.jpg",
+        "head": "Skoda Octavia A5 restyle",
+        "text": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quis accusamus iste maiores aliquam autem rem voluptates necessitatibus. Cupiditate, nulla omnis. Rerum repudiandae doloribus inventore voluptatum harum nisi facilis eligendi!"
+    },
+    {
+        "id": "fabiaRS",
+        "url": "https://autoreview.ru/images/Article/10/Article_1068_860_575.jpg",
+        "head": "Skoda Fabia RS II generation",
+        "text": "quas vero rem pariatur, vel expedita incidunt aliquid dignissimos? Sequi, deleniti! Officiis voluptatum modi, ea aliquid aut illo deleniti vitae incidunt, numquam, similique dolore reprehenderit dolor neque? Iure! Excepturi laboriosam quos porro molestiae"
+    },
+    {
+        "id": "superb",
+        "url": "https://dvizhok.su/i/files2/auto/2019/05/Skoda_Superb_2019_2.jpg",
+        "head": "Skoda Superb III generation restyle",
+        "text": "doloremque praesentium tempore ducimus libero ab nemo, distinctio voluptas quae aspernatur at cumque, natus dolor? Facilis vero exercitationem adipisci fuga nam cum obcaecati at maiores? Iure veritatis alias quasi ducimus recusandae dicta vero unde"
+    }
+]`
